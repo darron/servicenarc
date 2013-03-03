aa09417e6ac2816d6e3fac5744d6521762d6ebb5
Darron Froese <darron@froese.org>
2013-03-03 15:56:25 -0700

    Store and work with hits.

--- models/service_hit.rb
+++ models/service_hit.rb
@@ -1,11 +1,33 @@
 class ServiceHit
 
   def store(url, node, ip, time)
-    $redis.sadd "#{url}", "#{node}"
+    $redis.multi do
+      $redis.sadd "#{url}", "#{node}"
+      $redis.lpush "#{url}-#{node}", "#{time}"
+      $redis.ltrim "#{url}-#{node}", 0, 29      
+    end
   end
 
   def self.list_members(url)
-    $redis.smembers "#{url}"
+    string = String.new
+    nodes = $redis.smembers "#{url}"
+  end
+
+  def self.node_length(url, node)
+    $redis.llen "#{url}-#{node}"
+  end
+
+  def self.last_checkin(url, node)
+    $redis.lindex "#{url}-#{node}", 0
+  end
+
+  def self.nodes_checkin_list(url, nodes)
+    checkins = Hash.new
+    nodes.each do |node|
+      last = self.last_checkin(url, node)
+      checkins[node] = last
+    end
+    return checkins
   end
 
 end
