ae5482e2e6e3874d04226c7d360cdf6a14dbbc0a
Darron Froese <darron@froese.org>
2013-03-04 12:39:08 -0700

    Basic service status page.

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -1,6 +1,15 @@
 Admin.controllers :base do
 
   get :index, :map => "/" do
+  	@services = Service.all
+  	@nodes = Hash.new
+  	@checkins = Hash.new
+  	@services.each do |service|
+  		@nodes[service.url] = ServiceHit.list_members(service.url)
+  	end
+  	@nodes.each do |url, node|
+  		@checkins[url] = ServiceHit.nodes_checkin_list(url, node)
+  	end
     render "base/index"
   end
 end
